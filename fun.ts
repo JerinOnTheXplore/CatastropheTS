101a9f664d5512ae58ddb671c0ea4c98cdbbd56f
JerinXplore <jerinjerin101325@gmail.com>
2025-11-29 20:51:40 +0600

    Created doraemonTimeTravel function with validation, year aggregation, direction-based computation, and pre-1969 paradox warning

--- fun.ts
+++ fun.ts
@@ -8,13 +8,12 @@ You see a cat on the street. Cats can be Happy, Hungry, or Sleepy. Create a func
 type CatMood = "Happy" | "Hungry" | "Sleepy";
 
 const checkCatMood = (catName: string, mood: CatMood): string =>
-  `${catName} ${
-    mood === "Happy"
-      ? "purrs happily!"
-      : mood === "Hungry"
-      ? "meows loudly! Feed it quickly!"
-      : "curls up and sleeps."
-  }`;
+    `${catName} ${mood === "Happy"
+        ? "purrs happily!"
+        : mood === "Hungry"
+            ? "meows loudly! Feed it quickly!"
+            : "curls up and sleeps."
+    }`;
 
 console.log(checkCatMood("Shondesh", "Hungry"));
 //output: Shondesh meows loudly! Feed it quickly!
@@ -35,8 +34,8 @@ Bangladesh is earthquake-prone. Create a "earthquakeAlert" function that:
 4. Include a message about surviving the earthquake.
 */
 
-function earthquakeAlert(location:string, magnitude:number):string {
-    if (magnitude > 5){
+function earthquakeAlert(location: string, magnitude: number): string {
+    if (magnitude > 5) {
         return `Warning!! Earthquake detected in ${location} with magnitude ${magnitude}. Stay safe and hold onto something sturdy!!`
     } else {
         return `Earthquake detected in ${location} with magnitude ${magnitude}. All safe and no need to wory!`
@@ -66,8 +65,8 @@ If no subject is provided, it should return:
 "You are doing itraamii too much! Add some subjects first!!"
 */
 
-const examSubjectsTracker = (studentName: string, ...subjects: string[]): string =>{
-    if(subjects.length === 0){
+const examSubjectsTracker = (studentName: string, ...subjects: string[]): string => {
+    if (subjects.length === 0) {
         return `${studentName}, you are doing itraamii too much! Add some subjects first!!`
     }
     const subjectList = subjects.join(", ");
@@ -87,26 +86,26 @@ You pass your “mood” to the robot and the robot suggests a food.
 
 Create a function foodDecisionBot(mood: string) that:
 
-If mood = "happy" → return "Eat biriyani! Celebrate your joy!"
+If mood = "happy" => return "Eat biriyani! Celebrate your joy!"
 
-If mood = "sad" → return "Eat ice cream… emotions > diet!"
+If mood = "sad" => return "Eat ice cream… emotions > diet!"
 
-If mood = "lazy" → return "Order pizza. You won't cook anyway."
+If mood = "lazy" => return "Order pizza. You won't cook anyway."
 
-For any other mood → return "Just drink water. Confused feelings detected."
+For any other mood => return "Just drink water. Confused feelings detected."
 
 Mood must be typed strictly using a union type.
 */
 
-type Mood = "happy"| "angry" | "sad" | "lazy";
+type Mood = "happy" | "angry" | "sad" | "lazy";
 
 function foodDecisionBot(mood: Mood): string {
     if (mood === "happy") {
         return "I will biriyani khaboooo!";
     }
-    else if (mood === "sad"){
+    else if (mood === "sad") {
         return "Eat ice cream… emotions > diet!!"
-    }else if (mood === "angry"){
+    } else if (mood === "angry") {
         return "football banay ekta kick dibo..then I will eat sth spicy"
     }
     else {
@@ -118,3 +117,91 @@ console.log(foodDecisionBot("angry"));// football banay ekta kick dibo..then I w
 console.log(foodDecisionBot("happy"));//I will biriyani khaboooo!
 
 console.log(foodDecisionBot("lazy"));// vallagena!!Order pizza!!
+
+
+/*
+Problem 6 – Doraemon’s Time Travel 
+
+Nobita has messed up his homework again !!
+So Doraemon brought out his Time Machine to help (or make things worse).
+
+Create a TypeScript function doraemonTimeTravel that:
+
+ Requirements
+
+1. The function should accept:
+
+   A starting year (number)
+
+   A direction: "future" or "past"
+
+   Any number of years to travel using the rest operator
+
+Example:
+doraemonTimeTravel(2025, "future", 10, 5, 3)
+
+2. Doraemon will:
+
+ i)Add all the years if the direction is "future"
+
+ ii)Subtract all the years if the direction is "past"
+
+3. The function should finally return a fun message such as:
+
+ i) "Doraemon took you to the year 2043! Still no bamboo-copter upgrade."
+
+ ii)"You travelled back to 1995! Tell Nobita's parents to buy a computer!"
+
+4. If no years are provided, return:
+
+ "DoraemonError: You didn’t enter any years to travel! Please try again."
+
+5. If the direction is invalid, return:
+
+ "Invalid direction! Doraemon says: choose 'future' or 'past' only!"
+
+6. Add a bonus twist:
+
+If the result year is before the invention of Doraemon (i.e., before 1969),
+return an additional line:
+"Alert! You travelled before Doraemon was created. Time paradox detected!"
+*/
+
+
+function doraemonTimeTravel(
+  startYear: number,
+  direction: "future" | "past",
+  ...years: number[]
+): string {
+  if (years.length === 0) {
+    return "DoraemonError: You didn’t enter any years to travel! Please try again.";
+  }
+
+  if (direction !== "future" && direction !== "past") {
+    return "Invalid direction! Doraemon says: choose 'future' or 'past' only!";
+  }
+
+  const totalYears = years.reduce((sum, y) => sum + y, 0);
+
+  const finalYear =
+    direction === "future" ? startYear + totalYears : startYear - totalYears;
+
+  let message =
+    direction === "future"
+      ? `Doraemon took you to the year ${finalYear}! Still no bamboo-copter upgrade.`
+      : `You travelled back to the year ${finalYear}! Tell Nobita's parents to buy a computer!`;
+
+  // Bonus warning
+  if (finalYear < 1969) {
+    message =
+      message +
+      "\nAlert! You travelled before Doraemon was created. Time paradox detected!";
+  }
+
+  return message; 
+}
+
+console.log(doraemonTimeTravel(2025, "future", 10, 5, 3));//Doraemon took you to the year 2043! Still no bamboo-copter upgrade.
+console.log(doraemonTimeTravel(2025, "past", 30, 50));//You travelled back to the year 1945! Tell Nobita's parents to buy a computer!
+// Alert! You travelled before Doraemon was created. Time paradox detected!
+console.log(doraemonTimeTravel(2025, "future"));//DoraemonError: You didn’t enter any years to travel! Please try again.
